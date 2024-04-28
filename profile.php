6be7be9ad70230602a8e0fb80fb04b7163d2c188
rg3pzr <rg3pzr@virginia.edu>
2024-04-28 16:48:35 -0400

    profile.php

--- profile.php
+++ profile.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
+
 $userId = $_SESSION['user_id'];
 $userStmt = $db->prepare("SELECT username FROM Users WHERE user_id = ?");
 $userStmt->execute([$userId]);
